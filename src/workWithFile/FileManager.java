bee874acf1e4d0139d44abd4d166dd0b937b29bc
Delsa <edelvament@gmail.com>
2021-03-01 13:12:56 +0200

    Update ParserJSON, FileManager.

--- src/workWithFile/FileManager.java
+++ src/workWithFile/FileManager.java
@@ -1,8 +1,66 @@
 package workWithFile;
 
-public class FileManager {
-    public void writeInFile(String element){
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+/**
+ * Класс FileManager, отвечающий за работу с файлом.
+ * @author Delsa
+ * @version 1.0
+ */
+
+public class FileManager {
+    /** Поле файл */
+    private File file;
+
+    public FileManager() {
+        this.file = new File(System.getenv("LAB"));
+    }
+
+    /**
+     * Функция записи в файл{@link FileManager#file}
+     * @param data - данные для записи в файл.
+     */
+
+    private void writeInFile(String data){
+        FileWriter writer = null;
+        try{
+            writer = new FileWriter(file);
+            writer.write(data);
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                writer.close();
+            }catch (IOException e){
+                e.printStackTrace();
+
+            }
+        }
+
+    }
+
+    /**
+     * Функция для чтения из файла{@link FileManager#file}
+     * @return Возвращает строку данных.
+     */
+
+    private String readFromFile(){
+        List<String> data = new ArrayList<String>();
+        try {
+            Scanner scanner = new Scanner(file);
+            while(scanner.hasNextLine()){
+                data.add(scanner.nextLine());
+            }
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        return data.toString();
     }
 
 
--- src/workWithFile/ParserJSON.java
+++ src/workWithFile/ParserJSON.java
@@ -1,17 +1,42 @@
 package workWithFile;
 
+import collection.Car;
+import collection.Coordinates;
 import collection.HumanBeing;
+import collection.Mood;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+/**
+ * Интерфейс для работы с JSON.
+ * @author Delsa
+ * @version 1.0
+ */
 
 public interface ParserJSON {
-    static HumanBeing readFromJson(String element){
 
+    /**
+     * Метод для конвертирования строки в JSON.
+     * @param data - строка данных
+     * @return Возвращает объект класса {@link HumanBeing#HumanBeing(String, Coordinates, Boolean, Boolean, long, String, Integer, Mood, Car)}
+     */
 
-        return null;
+    static HumanBeing readFromJson(String data){
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        return gson.fromJson(data, HumanBeing.class);
     }
 
-    static String writeInJson(HumanBeing humanBeing){
+    /**
+     * Метод для конвертации класса
+     * @param humanBeing - объект класса {@link HumanBeing#HumanBeing(String, Coordinates, Boolean, Boolean, long, String, Integer, Mood, Car)}
+     * @return Возвращает JSON строку.
+     */
 
-        return null;
+    static String writeInJson(HumanBeing humanBeing){
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        return gson.toJson(humanBeing);
     }
 
 
