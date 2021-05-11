ab755ae9081cdf8cc929bb8354e652cb2f6985cc
Delsa <51116291+Edelveysa@users.noreply.github.com>
2021-05-11 11:21:35 +0300

    Update AddIfMaxCommand.java

--- src/сommands/AddIfMaxCommand.java
+++ src/сommands/AddIfMaxCommand.java
@@ -5,18 +5,31 @@ import exceptions.EmptyExecuteArgumentException;
 import managers.CollectionManager;
 import managers.HumanBeingBuilder;
 
+/**
+ * Класс AddIfMaxCommand.
+ * Команда "add_if_max" - добавляет новый элемент в коллекцию, если скорость удара наибольшая.
+ *
+ * @version 1.2
+ */
 
 public class AddIfMaxCommand extends AbstractCommand
 {
+    /** Поле объект менеджера коллекции */
     private CollectionManager collectionManager;
+    /** Поле объект-строитель человека */
     private HumanBeingBuilder builder;
 
     public AddIfMaxCommand(CollectionManager collectionManager, HumanBeingBuilder builder)
     {
-        super("add_if_max", "Добавляет новый элемент в коллекцию, если сила удара наибольшая.");
+        super("add_if_max", "Добавляет новый элемент в коллекцию, если скорость удара наибольшая.");
         this.builder = builder;
         this.collectionManager = collectionManager;
     }
+
+    /**
+     * Исполнение команды.
+     * @return Статус выполнения команды.
+     */
 
     @Override
     public boolean execute(String arg)
