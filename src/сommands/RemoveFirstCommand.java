ae20f3f11bc7e1ae9e04ec28bbb6fccdea01682d
Delsa <51116291+Edelveysa@users.noreply.github.com>
2021-05-11 11:24:14 +0300

    Update RemoveFirstCommand.java

--- src/сommands/RemoveFirstCommand.java
+++ src/сommands/RemoveFirstCommand.java
@@ -2,14 +2,28 @@ package сommands;
 
 import managers.CollectionManager;
 
+/**
+ * Класс RemoveFirstCommand.
+ * Команда "remove_first" - удаление первого элемента в коллекции.
+ *
+ * @version 1.2
+ */
+
 public class RemoveFirstCommand extends AbstractCommand
 {
+    /** Поле объект менеджера коллекции */
     private CollectionManager collectionManager;
+
     public RemoveFirstCommand(CollectionManager collectionManager)
     {
         super("remove_first", "Удалить первый элемент в коллекции.");
         this.collectionManager = collectionManager;
     }
+
+    /**
+     * Исполнение команды.
+     * @return Статус выполнения команды.
+     */
 
     @Override
     public boolean execute() {
