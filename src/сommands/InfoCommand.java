d60dd10ecdcd24d9c49bcf19247bb66727d298e2
Delsa <51116291+Edelveysa@users.noreply.github.com>
2021-05-11 11:23:35 +0300

    Update InfoCommand.java

--- src/сommands/InfoCommand.java
+++ src/сommands/InfoCommand.java
@@ -2,15 +2,28 @@ package сommands;
 
 import managers.CollectionManager;
 
+/**
+ * Класс InfoCommand.
+ * Команда "info" - вывод информации о коллекции.
+ *
+ * @version 1.2
+ */
+
 public class InfoCommand extends AbstractCommand
 {
+    /** Поле объект менеджера коллекции */
     private CollectionManager collectionManager;
+
     public InfoCommand(CollectionManager collectionManager)
     {
         super("info", "Вывод информации о коллекции");
         this.collectionManager = collectionManager;
-
     }
+
+    /**
+     * Исполнение команды.
+     * @return Статус выполнения команды.
+     */
 
     @Override
     public boolean execute()
