91c912558a06e6c54558bf723fb3cec9927e16d4
Delsa <51116291+Edelveysa@users.noreply.github.com>
2021-05-11 11:22:27 +0300

    Update ExecuteScriptCommand.java

--- src/сommands/ExecuteScriptCommand.java
+++ src/сommands/ExecuteScriptCommand.java
@@ -2,16 +2,24 @@ package сommands;
 
 import exceptions.EmptyExecuteArgumentException;
 
+/**
+ * Класс ExecuteScriptCommand.
+ * Команда "execute_script" - исполнение скрипт из указанного файла.
+ *
+ * @version 1.2
+ */
+
 public class ExecuteScriptCommand extends AbstractCommand
 {
     public ExecuteScriptCommand() {
-        super("execute_script filename", "исполнить скрипт из указанного файла");
+        super("execute_script filename", "Исполнение скрипта из указанного файла");
     }
 
     /**
      * Выполняет скрипт.
      * @return Статус выполнения команды.
      */
+
     @Override
     public boolean execute(String arg) {
         try {
