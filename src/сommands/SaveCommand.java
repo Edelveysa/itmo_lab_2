dbc81748a0b6f812553db760e08ea8ded4750722
Delsa <51116291+Edelveysa@users.noreply.github.com>
2021-05-11 11:24:31 +0300

    Update SaveCommand.java

--- src/сommands/SaveCommand.java
+++ src/сommands/SaveCommand.java
@@ -2,14 +2,28 @@ package сommands;
 
 import managers.CollectionManager;
 
+/**
+ * Класс SaveCommand.
+ * Команда "save" - сохранение коллекции в файл.
+ *
+ * @version 1.2
+ */
+
 public class SaveCommand extends AbstractCommand
 {
+    /** Поле объект менеджера коллекции */
     private CollectionManager collectionManager;
+
     public SaveCommand(CollectionManager collectionManager)
     {
         super("save", "Сохраняет коллекцию в файл.");
         this.collectionManager = collectionManager;
     }
+
+    /**
+     * Исполнение команды.
+     * @return Статус выполнения команды.
+     */
 
     @Override
     public boolean execute()
