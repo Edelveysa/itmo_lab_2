8fcaf2e49ef2ba6da4836461fc750bcb8c124eee
Delsa <51116291+Edelveysa@users.noreply.github.com>
2021-05-11 11:24:44 +0300

    Update ShowCommand.java

--- src/сommands/ShowCommand.java
+++ src/сommands/ShowCommand.java
@@ -4,14 +4,28 @@ package сommands;
 import exceptions.EmptyCollectionException;
 import managers.CollectionManager;
 
+/**
+ * Класс ShowCommand.
+ * Команда "show" - вывод всех элементов коллекции.
+ *
+ * @version 1.2
+ */
+
 public class ShowCommand extends AbstractCommand
 {
+    /** Поле объект менеджера коллекции */
     private CollectionManager collectionManager;
+
     public ShowCommand(CollectionManager collectionManager)
     {
         super("show", "Выводит все элементы коллекции.");
         this.collectionManager = collectionManager;
     }
+
+    /**
+     * Исполнение команды.
+     * @return Статус выполнения команды.
+     */
 
     @Override
     public boolean execute() {
