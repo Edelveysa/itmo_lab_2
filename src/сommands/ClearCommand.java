998226881de6dc65d7e032833cc8cd4cb0b375c5
Delsa <51116291+Edelveysa@users.noreply.github.com>
2021-05-11 11:21:52 +0300

    Update ClearCommand.java

--- src/сommands/ClearCommand.java
+++ src/сommands/ClearCommand.java
@@ -4,14 +4,28 @@ package сommands;
 import exceptions.EmptyCollectionException;
 import managers.CollectionManager;
 
+/**
+ * Класс ClearCommand.
+ * Команда "clear" - очищает коллекцию.
+ *
+ * @version 1.2
+ */
+
 public class ClearCommand extends AbstractCommand
 {
+    /** Поле объект менеджера коллекции */
     private CollectionManager collectionManager;
+
     public ClearCommand(CollectionManager collectionManager)
     {
         super("clear", "Очищает коллекцию.");
         this.collectionManager = collectionManager;
     }
+
+    /**
+     * Исполнение команды.
+     * @return Статус выполнения команды.
+     */
 
     @Override
     public boolean execute()
