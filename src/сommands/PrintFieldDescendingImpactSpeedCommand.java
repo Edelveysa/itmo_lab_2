fde78f4c82affcaf92132ba11c475a4b6a769935
Delsa <51116291+Edelveysa@users.noreply.github.com>
2021-05-11 11:23:49 +0300

    Update PrintFieldDescendingImpactSpeedCommand.java

--- src/сommands/PrintFieldDescendingImpactSpeedCommand.java
+++ src/сommands/PrintFieldDescendingImpactSpeedCommand.java
@@ -3,13 +3,25 @@ package сommands;
 import exceptions.EmptyCollectionException;
 import managers.CollectionManager;
 
+/**
+ * Класс PrintFieldDescendingImpactSpeedCommand.
+ * Команда "print_field_descending_impact_speed" - вывод значений всех элементов в порядке убывания скорости удара.
+ *
+ * @version 1.2
+ */
+
 public class PrintFieldDescendingImpactSpeedCommand extends AbstractCommand
 {
     private CollectionManager collectionManager;
     public PrintFieldDescendingImpactSpeedCommand(CollectionManager collectionManager){
-        super("print_field_descending_impact_speed", "Выводит значения ImpactSpeed всех элементов в порядке убывания.");
+        super("print_field_descending_impact_speed", "Выводит значения всех элементов в порядке убывания скорости удара.");
         this.collectionManager = collectionManager;
     }
+
+    /**
+     * Исполнение команды.
+     * @return Статус выполнения команды.
+     */
 
     @Override
     public boolean execute() {
