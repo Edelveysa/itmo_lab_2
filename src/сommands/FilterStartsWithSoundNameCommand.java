c3cf742c8dd6ffda3f2c549e55cbf6b08e4a3130
Delsa <51116291+Edelveysa@users.noreply.github.com>
2021-05-11 11:23:08 +0300

    Update FilterStartsWithSoundNameCommand.java

--- src/сommands/FilterStartsWithSoundNameCommand.java
+++ src/сommands/FilterStartsWithSoundNameCommand.java
@@ -3,14 +3,28 @@ package сommands;
 import exceptions.EmptyExecuteArgumentException;
 import managers.CollectionManager;
 
+/**
+ * Класс FilterStartsWithSoundNameCommand.
+ * Команда "filter_starts_with_soundtrack_name" - вывод элементов, где название саундрека начинается с заданной подстроки.
+ *
+ * @version 1.2
+ */
+
 public class FilterStartsWithSoundNameCommand extends AbstractCommand
 {
+    /** Поле объект менеджера коллекции */
     private CollectionManager collectionManager;
+
     public FilterStartsWithSoundNameCommand(CollectionManager collectionManager)
     {
-        super("filter_starts_with_soundtrack_name name", "Вывести элементы, где название саундрека начинается с заданной подстроки.");
+        super("filter_starts_with_soundtrack_name name", "Вывод элементов, где название саундрека начинается с заданной подстроки.");
         this.collectionManager = collectionManager;
     }
+
+    /**
+     * Исполнение команды.
+     * @return Статус выполнения команды.
+     */
 
     @Override
     public boolean execute(String arg) {
