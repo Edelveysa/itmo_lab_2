462571c3d9a85fd1cf3c1e952ecf0f0e1e605fa4
Delsa <51116291+Edelveysa@users.noreply.github.com>
2021-05-11 11:21:09 +0300

    Update AbstractCommand.java

--- src/сommands/AbstractCommand.java
+++ src/сommands/AbstractCommand.java
@@ -2,10 +2,17 @@ package сommands;
 
 import java.util.Objects;
 
+/**
+ * Абстрактный класс AbstractCommand, класс-родитель для всех создаваемых команд.
+ *
+ * @version 1.2
+ */
+
 public abstract class AbstractCommand
 {
-
+    /** Поле имя команды */
     private String name;
+    /** Поле описание команды */
     private String description;
 
     public AbstractCommand(String name, String description)
@@ -14,15 +21,26 @@ public abstract class AbstractCommand
         this.description = description;
     }
 
+    /**
+     * Выполнение команды с аргументом.
+     * @param argument
+     * @return false
+     */
+
     public boolean execute(String argument)
     {
         return false;
     };
+
+    /**
+     * Выполнение команды без аргументов.
+     * @return
+     */
+
     public boolean execute()
     {
         return false;
     }
-
 
     /**
      * @return Возвращает имя команды.
@@ -38,6 +56,11 @@ public abstract class AbstractCommand
     public String getDescription() {
         return description;
     }
+
+    /**
+     * Установка описания команды.
+     * @param description
+     */
 
     public void setDescription(String description)
     {
