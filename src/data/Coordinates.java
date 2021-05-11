39e5c23f6d9c1700a8d444ccad72c377209c0f86
Delsa <51116291+Edelveysa@users.noreply.github.com>
2021-05-11 11:18:36 +0300

    Update Coordinates.java

--- src/data/Coordinates.java
+++ src/data/Coordinates.java
@@ -1,10 +1,16 @@
 package data;
 
-import exceptions.MaxValueExceededException;
+/**
+ * Класс Coordinates, класс, который дает координаты (X; Y) человеку.
+ *
+ * @version 1.0
+ */
 
 public class Coordinates
 {
+    /**Поле координата Х*/
     private Integer x; //Максимальное значение поля: 841, Поле не может быть null
+    /**Поле координата Y*/
     private Double y; //Поле не может быть null
 
     public Coordinates(Integer x, Double y)
@@ -13,20 +19,38 @@ public class Coordinates
         setY(y);
     }
 
+    /**
+     * Установка координаты Х.
+     * @param x
+     */
+
     public void setX(Integer x)
     {
         this.x = x;
     }
+
+    /**
+     * Установка координаты Y.
+     * @param y
+     */
 
     public void setY(Double y)
     {
         this.y = y;
     }
 
+    /**
+     * @return Координата Х.
+     */
+
     public Integer getX()
     {
         return x;
     }
+
+    /**
+     * @return Координата Y.
+     */
 
     public Double getY()
     {
